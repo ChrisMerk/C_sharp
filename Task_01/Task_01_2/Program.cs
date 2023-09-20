84e0be04abf1eae0536fb8fdaea9e68027fba425
Mikey <theneworder319@gmail.com>
2023-09-20 22:30:49 +0300

    Сделал задание по 2му семинару

---
+++ Task_01/Task_01_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.WriteLine("Напишите число: ");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+if (num > 99)
+{
+    while (num > 999)
+    {
+        num /= 10;
+    }
+    Console.WriteLine(num % 10);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
---
+++ Task_01/Task_01_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.WriteLine("Напишите число: ");
+string strNum = Console.ReadLine();
+int num = int.Parse(strNum);
+if (num == 6 || num == 7)
+{
+    Console.WriteLine("выходной");
+}
+else
+{
+    Console.WriteLine("не выходной");
+}
